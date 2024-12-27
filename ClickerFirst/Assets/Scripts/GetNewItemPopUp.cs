f062b9c7b06f32044d5c09d71aa5a7c7b15a2eb2
DimaD <davidovda1990@gmail.com>
2024-12-27 22:41:55 +0500

    #18

--- ClickerFirst/Assets/Scripts/GetNewItemPopUp.cs
+++ ClickerFirst/Assets/Scripts/GetNewItemPopUp.cs
@@ -9,36 +9,54 @@ public class GetNewItemPopUp : MonoBehaviour
     private string currGroup;
 
     private int currEquipN;
-    private Image imgSpriteEquip;
-    private string isItemEquippedName;
+
+    [SerializeField] private Image imgSpriteEquip; // Теперь можно задать через инспектор
     [SerializeField] private Button btnEquipItem;
-    
-    public static event Action<string,int> OnItemEquipped = delegate(string _equipGroup, int _equipN) { };
-    public static event Action<string,int,Image> OnNeedFindSprite = delegate(string _equipGroup, int _equipN, Image _imageSprite) { };
+
+    private string isItemEquippedName;
+
+    public static event Action<string, int> OnItemEquipped = delegate (string _equipGroup, int _equipN) { };
+    public static event Action<string, int, Image> OnNeedFindSprite = delegate (string _equipGroup, int _equipN, Image _imageSprite) { };
+
     // Start is called before the first frame update
     void Start()
     {
-        imgSpriteEquip = FindImageAmongChildren(transform);
-        btnEquipItem.onClick.AddListener(SaveDataAndEquip);
-        
-        
+        // Если imgSpriteEquip не задан через инспектор, ищем его среди дочерних объектов
+        if (imgSpriteEquip == null)
+        {
+            imgSpriteEquip = FindImageAmongChildren(transform);
+
+            if (imgSpriteEquip == null)
+            {
+                Debug.LogError("Image не найден среди дочерних объектов и не задан в инспекторе.");
+            }
+        }
+
+        // Назначаем слушатель кнопки
+        if (btnEquipItem != null)
+        {
+            btnEquipItem.onClick.AddListener(SaveDataAndEquip);
+        }
+        else
+        {
+            Debug.LogError("btnEquipItem не назначен в инспекторе.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void ShowGetNewItemPopUp(string group, int equip)
     {
-       // Debug.Log("ShowGetNewItemFunct");
         currGroup = group;
         currEquipN = equip;
-       
+
+        // Вызываем событие для обработки отображения
         OnNeedFindSprite(currGroup, currEquipN, imgSpriteEquip);
     }
-    
+
     private Image FindImageAmongChildren(Transform parent)
     {
         // Перебираем только первый уровень дочерних объектов
@@ -53,7 +71,7 @@ public class GetNewItemPopUp : MonoBehaviour
         }
         return null; // Если Image не найден
     }
-    
+
     private void SaveDataAndEquip()
     {
         isItemEquippedName = $"ItemEquipped_{currGroup}_N_";
