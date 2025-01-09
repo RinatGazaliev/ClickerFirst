16f1813ef4a117629740dfbcd3f1deb23559c94d
DimaD <davidovda1990@gmail.com>
2025-01-09 15:23:22 +0500

    #20 PreparedForParalax

--- ClickerFirst/Assets/Scripts/Config.cs
+++ ClickerFirst/Assets/Scripts/Config.cs
@@ -113,7 +113,7 @@ public class Config : MonoBehaviour
     #region ChangeRoadAbdHeavenTexture
 
     private const string CURRENTTEXTUREROADNUMBER = "TextureRoad_N";
-    public static readonly int[] DistanceToChangeTextureRoad = { 25, 50, 100, 200, 400 };
+    public static readonly int[] DistanceToChangeTextureRoad = { 25, 75, 150, 300, 500, 750, 1050, 1400, 1800, 2250, 2750, 3300 };
     //private bool isHeavenMove = false;
     public static event Action<bool> OnChangeHeavenMove = delegate (bool _isHeaveMove) { };
 
