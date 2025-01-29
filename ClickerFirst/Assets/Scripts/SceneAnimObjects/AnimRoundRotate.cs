4a2e62cc4ed218f6f4acbc5a77e6b9a99fae3bed
RinatGazaliev <123339016+RinatGazaliev@users.noreply.github.com>
2025-01-29 20:18:25 +0500

    #AnimRotateSomeCorrection

--- ClickerFirst/Assets/Scripts/SceneAnimObjects/AnimRoundRotate.cs
+++ ClickerFirst/Assets/Scripts/SceneAnimObjects/AnimRoundRotate.cs
@@ -5,7 +5,7 @@ using DG.Tweening;
 
 public class AnimRoundRotate : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 360.0f; // Скорость вращения (градусы в секунду)
+    [SerializeField] private float fullAnimCycleTime = 360.0f; // Время в сек на 1 оборот
     [SerializeField] private float startDelay = 0.5f;       // Задержка перед началом вращения
 
     private void Start()
@@ -16,7 +16,7 @@ public class AnimRoundRotate : MonoBehaviour
     private void StartRotation()
     {
         // Анимация вращения по часовой стрелке
-        transform.DORotate(new Vector3(0, 0, -360), rotationSpeed, RotateMode.FastBeyond360)
+        transform.DORotate(new Vector3(0, 0, -360), fullAnimCycleTime, RotateMode.FastBeyond360)
             .SetDelay(startDelay)
             .SetEase(Ease.Linear)
             .SetLoops(-1, LoopType.Restart);
